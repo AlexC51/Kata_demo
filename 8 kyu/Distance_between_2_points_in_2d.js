15c82953ca3a571e24b9f8663694a331c334dcec
AlexC51 <godsdog51@gmail.com>
2021-03-20 19:49:09 +0200

    Update Distance_between_2_points_in_2d.js
    Created point class

--- 8 kyu/Distance_between_2_points_in_2d.js
+++ 8 kyu/Distance_between_2_points_in_2d.js
@@ -8,7 +8,24 @@ Write a function calculating distance between Point a and Point b.
 Tests round answers to 6 decimal places.
 */
 
+class Point
+{
+    constructor (x,y)
+    {
+this.x = x;
+this.y = y;
+    }
+}
 
-function distanceBetweenPoints(a, b) {
-    // your code here
+
+
+  function distanceBetweenPoints(a, b) {
+    var XD;
+    if (a.x > b.x )
+    XD = (a.x - b.x)
+    else XD = (b.x-a.x)
+    return XD
   }
+
+var dist = distanceBetweenPoints (7,9)
+console.log(dist);
